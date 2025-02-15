a396ea987adec3bf7b4a2215205a465c78571f6d
belkud <vlad-belkud@yandex.ru>
2025-02-15 21:33:12 +0300

    Learn_JS

---
+++ script.js
@@ -0,0 +1,93 @@
+
+
+
+
+var x = 10
+console.log(x)
+// var - устаревшие названия переменных (лучше их не писать)
+// let - нельзя чтобы две переменные были под одним названием, но мы можем менять их значения
+// const - константа, значения неизменны
+
+var x = 12
+console.log(x)
+
+
+
+
+const z = 25
+
+console.log(z);
+
+let y = 6
+let y2 = 3
+
+console.log(y+y2)
+console.log(y-y2)
+console.log(y*y2)
+console.log(y/y2)
+console.log(y**2) // возведение числа в степень
+console.log(y*y) // возведение числа в степень
+
+console.log(y%y2) // остаток от деления 
+
+
+let whiteColorWithBlackColor
+let white_color_with_black_color
+
+let a = 5
+console.log(typeof a)
+
+let b = 'abc'
+console.log(typeof b)
+
+let c
+console.log(typeof c)
+
+let d = 'abc' * 5
+console.log(d)
+
+console.log(y==y2); //'>' '<' '>=' '<=' '==' сравнение переменных (boolean)
+ 
+// let surname = prompt('Введите ваше имя')
+// console.log(surname)
+
+// let age = alert ('Ваш возраст')
+
+
+
+// let number1 = +prompt('Введите первое число')
+// let number2 = +prompt('Введите второе число')
+
+// alert( 'сумма чисел = ' + (number1 + number2))
+
+
+// console.log(number1 + number2)
+
+let j = 5554356547568
+console.log('переменная j = ' + j)
+
+
+// Модуль 1.
+
+// Задание 1. Запросите у пользователя его имя и выведите в ответ:
+// «Привет, его имя!».
+
+
+
+
+
+// Задание 2. Запросите у пользователя год его рождения, посчитайте,
+// сколько ему лет и выведите результат. Текущий год укажите
+// в коде как константу
+
+
+// let side = +prompt('Введите сторону квадрата')
+// console.log(side*4)
+
+
+// промпт
+
+console.log('вам '+ (2025 - 'год рождения пользователя'))
+
+
+
